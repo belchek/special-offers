8b975e38799d2775a31365f1dae4142a2be5b81d
belchek <belousov925@mail.ru>
2021-07-27 14:22:46 +0300

    Close projects

--- js/app.js
+++ js/app.js
@@ -1,24 +1,35 @@
 'use strict';
 
-function addOffer(parentEl, offer) {
-    const words = ['дня', 'дней', 'день'];
-    const number = offer.remainingDays % 100;
-    let result = words [1];
-    let numberToGo = number % 10;
-    if (number >= 5 && number <= 20) {
-        result = words[1];
-    } else if (number) {
-        switch (numberToGo) {
+function convertToDaysString(offer) {
+    const speechForRemainingDays = ['дня', 'дней', 'день'];
+    const numeralToDays = offer.remainingDays % 100;
+    let resultToDaysString = speechForRemainingDays [1];
+    const numeralForFunction = numeralToDays % 10;
+    const minRange = 5;
+    const maxRange = 20;
+    if (numeralToDays >= minRange && numeralToDays <= maxRange) {
+        resultToDaysString = speechForRemainingDays[1];
+    } else if (numeralToDays) {
+        switch (numeralForFunction) {
             case (1):
-                result = words[2];
+                resultToDaysString = speechForRemainingDays[2];
                 break;
-            case (2):;
-            case (3):;
+            case (2):
+                resultToDaysString = speechForRemainingDays[0];
+                break;
+            case (3):
+                resultToDaysString = speechForRemainingDays[0];
+                break;
             case (4):
-                result = words[0];
+                resultToDaysString = speechForRemainingDays[0];
                 break;
         }
     }
+    return resultToDaysString;
+}
+
+function addOffer(parentEl, offer) {
+
     const rankEl = document.createElement('h3');
     rankEl.textContent = offer.title;
     rankEl.id = 'title';
@@ -39,26 +50,25 @@ function addOffer(parentEl, offer) {
         pictureEl.src = offer.image;
         pictureEl.width = 400;
         const daysEl = document.createElement('span');
-        daysEl.textContent = `Осталось (${result}): ${offer.remainingDays}`;
+        daysEl.textContent = `${offer.remainingDays} ${convertToDaysString(offer)}`;
         parentEl.appendChild(daysEl);
         const specificationEl = document.getElementById('description');
         specificationEl.textContent = offer.description;
     }
 }
 
-const newOffer = {
+const offer = {
     image: 'https://js-rosbank.ru/assets/offer-school.png',
     remainingDays: 0,
     title: 'Школьные выплаты на карту #МожноВСЁ',
     description: 'Получите 10000Р от государства +1500 от Росбанка',
 };
 
-const newOffer2 = {
-    image: 'https://js-rosbank.ru/assets/offer-visa-cashback.jpg',
-    remainingDays: 43,
-    title: 'Кешбэк 20% по картам VISA',
-    description: 'Оформите карту VISA #МожноВСЁ и получайте кешбэк 20% за покупки у наших партнеров',
-};
+// const newOffer2 = {
+//     image: 'https://js-rosbank.ru/assets/offer-visa-cashback.jpg',
+//     remainingDays: 21,
+//     title: 'Кешбэк 20% по картам VISA',
+//     description: 'Оформите карту VISA #МожноВСЁ и получайте кешбэк 20% за покупки у наших партнеров',
+// };
 
-
-addOffer(document.body, newOffer2);
+addOffer(document.body, offer);
